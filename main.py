c843eac17223d5b901b3483ea1549a3caed49198
tsars <tsarskiytsarskiy@mail.ru>
2025-04-15 00:14:51 +0700

    1

--- main.py
+++ main.py
@@ -1,93 +1,137 @@
-import json
 import os
-from collections import Counter
-
+import csv
+from tabulate import tabulate
+import textwrap
 
 FILE_PATH = './logs'
-TOTAL_lIST = []
-INFO_list, ERROR_list, WARNING_list, DEBUG_list, CRITICAL_list = [], [], [], [], []
-Total_requests = 0
+LOGS_lIST = []
+SET_lIST = []
+END_lIST = []
+TOTAL_INFO, TOTAL_ERROR, TOTAL_WARNING, TOTAL_DEBUG, TOTAL_CRITICAL, Total_requests = 0, 0, 0, 0, 0, 0
 
 
 class Analiz_logs():
-
     def load_file(self):
+        global SET_lIST, TOTAL_INFO, TOTAL_ERROR, TOTAL_WARNING, TOTAL_DEBUG, TOTAL_CRITICAL
+        Total_requests = 0
         files = os.listdir(FILE_PATH)
         for file in files:
-            # print(file, '********************************************************************************************************************************')
             with open(f'{FILE_PATH}/{file}', mode="r", encoding='utf-8') as file:
                 for line in file:
-                    global Total_requests
                     Total_requests += 1
-                    TOTAL_lIST.append(' '.join((line.split()[3:])))
-        for file in files:
-            with open(f'{FILE_PATH}/{file}', mode="r", encoding='utf-8') as file:
-                for line in file:
-                    # print(' '.join((line.split()[3:])))
-                    if ' '.join((line.split()[3:])) in set(TOTAL_lIST):
-                        # print(' '.join((line.split()[3:])))
-                        d = line.split()[2]
-                        print(d)
-
-        # with open(f'{FILE_PATH}/{file}', mode="r", encoding='utf-8') as file:
-        #     for line in file:
-        #         if line in set(TOTAL_lIST):
-        #             print(555555555)
-
-
-
-
-
-
-
-
-
-
-        #
-        #             if str(line.split()[2]) == 'INFO':
-        #                 INFO_list.append(' '.join((line.split()[3:])))
-        #             if str(line.split()[2]) == 'ERROR':
-        #                 ERROR_list.append(' '.join((line.split()[3:])))
-        #             if str(line.split()[2]) == 'WARNING':
-        #                 WARNING_list.append(' '.join((line.split()[3:])))
-        #             if str(line.split()[2]) == 'DEBUG':
-        #                 DEBUG_list.append(' '.join((line.split()[3:])))
-        #             if str(line.split()[2]) == 'CRITICAL':
-        #                 CRITICAL_list.append(' '.join((line.split()[3:])))
-        # print(Total_requests)
-        # # counter_INFO_dict = dict(Counter(INFO_list))
-        # print(type(TOTAL_lIST))
-        # print(type(' '.join((line.split()[3:]))))
-        # print(' '.join((line.split()[3:])))
-        # print(len(TOTAL_lIST))
-
-        # print(INFO_list)
-        # print(ERROR_list)
-        # print(WARNING_list)
-        # print(DEBUG_list)
-        # print(CRITICAL_list)
+                    LOGS_lIST.append([' '.join((line.split()[3:])), line.split()[2]])
+                    SET_lIST.append(' '.join((line.split()[3:])))
+        # END_lIST.append([f'Total requests:{Total_requests}'])
+        # END_lIST.append(['HANDLER', 'INFO', 'DEBUG', 'WARNING', 'ERROR', 'CRITICAL'])
+        SET_lIST = set(SET_lIST)
+        SET_lIST = list(SET_lIST)
+        SET_lIST = sorted(SET_lIST)
+        for i in SET_lIST:
+            INFO, ERROR, WARNING, DEBUG, CRITICAL = 0, 0, 0, 0, 0
+            for j in LOGS_lIST:
+                if i == j[0]:
+                    if j[1] == 'INFO':
+                        INFO += 1
+                        TOTAL_INFO += 1
+                    if j[1] == 'ERROR':
+                        ERROR += 1
+                        TOTAL_ERROR += 1
+                    if j[1] == 'WARNING':
+                        WARNING += 1
+                        TOTAL_WARNING += 1
+                    if j[1] == 'DEBUG':
+                        DEBUG += 1
+                        TOTAL_DEBUG += 1
+                    if j[1] == 'CRITICAL':
+                        CRITICAL += 1
+                        TOTAL_CRITICAL += 1
 
 
+            if '/' in i:
+                char_list = []
+                # print(char_list)
+                for ii, char in enumerate(i):
+                    # print(char)
+                    if char == "/":
+                        char_list.append(i.index(char))
+                        print(char_list)
+                print(type(char_list[0]), type(char_list[1]), type(char_list[2]))
+                i = i.split(i.index(min(char_list)))[1].split(i.index(max(char_list)))[0]
 
 
-#
-#                     b = ' '.join(a[3:])
-#
-#                     c = a[2] + ' ' + b
-#                     # c = list(a[2]).extend(list(b))
-#                     print(c)
-#
-# #
+            END_lIST.append([textwrap.fill(i, 25), INFO, DEBUG, WARNING, ERROR, CRITICAL])
+        END_lIST.append(['TOTAL_SUMM', TOTAL_INFO, TOTAL_ERROR, TOTAL_WARNING, TOTAL_DEBUG, TOTAL_CRITICAL])
+        # print(END_lIST)
+        # Запись в файл
+        with open('handlers.csv', 'w', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            writer.writerows(END_lIST)
+
+        # Чтение файла для проверки
+        with open('handlers.csv', 'r', encoding='utf-8') as file:
+            reader = csv.reader(file)
+            # for row in reader:
+            #     print(row)
 
 
 
 
+def print_table_tabulate(data):
+    # headers = ['HANDLER', 'INFO', 'DEBUG', 'WARNING', 'ERROR', 'CRITICAL']
+    formatted_data = ['HANDLER', 'INFO', 'DEBUG', 'WARNING', 'ERROR', 'CRITICAL']
 
+    # Вывод в разных стилях
+    # print("\nСтиль plain:")
+    # print(tabulate(END_lIST, tablefmt="plain"))
+    #
+    # print("\nСтиль grid:")
+    # print(tabulate(formatted_data, headers=headers, tablefmt="grid"))
+    #
+    # print("\nСтиль fancy_grid:")
+    # print(tabulate(formatted_data, headers=headers, tablefmt="fancy_grid"))
+    # assign data
+    # a = [
+    #     ["Nikhil", "Delhi"],
+    #     ["Ravi", "Kanpur"],
+    #     ["Manish", "Ahmedabad"],
+    #     ["Prince", "Bangalore"]
+    # ]
 
+    # create header
+    header = ["Name", "City"]
 
+    # print header
+    # print(f"{header[0]:<10} {header[1]:<15}")
+    # print("-" * 25)
+    line_width = 20
+    # print rows
+    # for row in END_lIST:
+        # print(f"{textwrap.fill(row[0], 30):<20} {row[1]:<15}  {row[2]:<15}  {row[3]:<15}  {row[4]:<15}  {row[5]:<15}")
 
+        # print('{:<30}|{:<10}|{:<10}|{:<10}|{:<10}|{:<10}'.format( row[0], row[1], row[2], row[3], row[4], row[5]))
+
+        # print(f'{row[0]}'.ljust(line_width) + "LEFT"*6, f'row[1]'.ljust(line_width) + "LEFT"*6, f'row[2]'.ljust(line_width) + "LEFT"*6, f'row[3]'.ljust(line_width) + "LEFT"*6)
+
+# textwrap.fill(row[0], 20)
+# "{0:<{1}}{0}".format(row[1], 40)
+# Пример использования
+# data = [
+#     [1, "Иван Петрович", 25],
+#     [2, "Мария Ивановна", 30]
+# ]
+# # textwrap.fill(row[0], 25),
 
 
 
 f = Analiz_logs()
 f.load_file()
+print_table_tabulate(END_lIST)
+
+
+# print ('{:<20}{:<20}'.format('UserName:ывпастм','Foo'))
+# print ('{:<30}{:<40}'.format('User:','FooBar'))
+# print ('{:<30}{:<40}'.format('','FooBar42'))
+#
+# line_width = 20
+# print ("Username:".ljust(line_width) + "LEFT"*6)
+# print ("".ljust(line_width) + "RIGHT"*3)
--- probe.py
+++ probe.py
@@ -1,165 +1,191 @@
-import os
-import csv
-import webbrowser
+# import os
+# import csv
+# import webbrowser
+#
+# from airium import Airium
+# import time
+# import keyboard
+#
+#
+# FILE_PATH = '../Price_List_Analyzer_Glob/Files_for_analysis'
+# FILE_HTML = '../Price_List_Analyzer_Glob/Files_for_analysis/output.html'
+# FILE_HTML_1 = 'output.html'
+# LIST_RESULT = []
+# SEARCH = input(f"Найти: ")
+# SAFE_WORD = []
+# class PriceMachine:
+#
+#     def load_prices(self):
+#         '''
+#             Сканирует указанный каталог. Ищет файлы со словом price в названии.
+#             В файле ищет столбцы с названием товара, ценой и весом.
+#             Допустимые названия для столбца с товаром:
+#                 товар
+#                 название
+#                 наименование
+#                 продукт
+#             Допустимые названия для столбца с ценой:
+#                 розница
+#                 цена
+#             Допустимые названия для столбца с весом (в кг.)
+#                 вес
+#                 масса
+#                 фасовка
+#         '''
+#         Product_name = ['товар', 'название', 'наименование', 'продукт']
+#         Name_with_price = ['розница', 'цена']
+#         Name_with_weight = ['вес', 'масса', 'фасовка']
+#         files = os.listdir(FILE_PATH)
+#
+#         for file in files:
+#
+#             # if key == "exit":
+#             #     break
+#             if "price" in file:
+#                 with open(f'{FILE_PATH}/{file}', mode="r", encoding='utf-8') as sort_file:
+#                     for row in csv.reader(sort_file):
+#                         for i in Product_name:
+#                             try:
+#                                 index_Product_name = row.index(i)
+#                             except:
+#                                 None
+#                         a = row[index_Product_name]
+#
+#                         for i in Name_with_price:
+#                             try:
+#                                 index_Name_with_price = row.index(i)
+#                             except:
+#                                 None
+#                         try:
+#                             b = int(row[index_Name_with_price])
+#                         except:
+#                             b = row[index_Name_with_price]
+#
+#                         for i in Name_with_weight:
+#                             try:
+#                                 index_Name_with_weight = row.index(i)
+#                             except:
+#                                 None
+#                         try:
+#                             c = int(row[index_Name_with_weight])
+#                         except:
+#                             c = row[index_Name_with_weight]
+#
+#                         if a not in Product_name and b not in Name_with_price and c not in Name_with_weight:
+#                             result = a, b, c, file
+#                             LIST_RESULT.append(result)
+#         LIST_RESULT.sort(key=lambda x: x[1])
+#         return LIST_RESULT
+#
+#     def find_text(self):
+#         '''
+#         Нолучает текст и возвращает список позиций,
+#         содержащий этот текст в названии продукта.
+#         <td 1></td><td Филе пангасиуса б/ш ></td><td 92></td><td 1></td><td price_5.csv></td>
+#         <td 2></td><td Филе пангасиуса б/ш ></td><td 103></td><td 1></td><td price_4.csv></td>
+#         <td 3></td><td Ряпушка вял н/р></td><td 119></td><td 1></td><td price_3.csv></td>
+#         <td 4></td><td Килька п/п ></td><td 130></td><td 1></td><td price_0.csv></td>
+#         '''
+#         file = open(f"{FILE_HTML}", "a", encoding='utf-8')
+#         b = Airium(source_minify=True)
+#         y = 0
+#
+#
+#         for i in LIST_RESULT:
+#
+#             # time.sleep(0.05)
+#             if keyboard.is_pressed('e'):
+#                 SAFE_WORD.append('e')
+#             if keyboard.is_pressed('x'):
+#                 SAFE_WORD.append('x')
+#             if keyboard.is_pressed('i'):
+#                 SAFE_WORD.append('i')
+#             if keyboard.is_pressed('t'):
+#                 SAFE_WORD.append('t')
+#             if {'e', 'x', 'i', 't'} <= set(SAFE_WORD):
+#                 break
+#
+#             if SEARCH in i[0]:
+#                 y += 1
+#                 b.break_source_line()
+#                 with b.tr():
+#                     with b.td(_t=y):
+#                         pass
+#                     with b.td(_t=i[0]):
+#                         pass
+#                     with b.td(_t=i[1]):
+#                         pass
+#                     with b.td(_t=i[2]):
+#                         pass
+#                     with b.td(_t=i[3]):
+#                         pass
+#         file.write(str(b))
+#         return b
+#
+#     def export_to_html(self):
+#         '''
+#         <!DOCTYPE html>
+#         <html>
+#         <head>
+#             <title>Позиции продуктов</title>
+#         </head>
+#         <body>
+#             <table>
+#                 <tr>
+#                     <th>Номер</th>
+#                     <th>Название</th>
+#                     <th>Цена</th>
+#                     <th>Фасовка</th>
+#                     <th>Файл</th>
+#                     <th>Цена за кг.</th>
+#                 </tr>
+#         '''
+#         file = open(f"{FILE_HTML}", "w", encoding ='utf-8')
+#         a = Airium()
+#         a('<!DOCTYPE html>')
+#         with ((((a.html())))):
+#             a.head()
+#             a.title(_t="Позиции продуктов")
+#         with a.body():
+#             with a.table():
+#                 with a.tr():
+#                     a.th(_t="№")
+#                     a.th(_t="Наименование")
+#                     a.th(_t="Цена")
+#                     a.th(_t="Масса")
+#                     a.th(_t="Файл")
+#                 with a.tr(_t=self.find_text()):
+#                     pass
+#             file.write(str(a))
+#         # webbrowser.open_new_tab(file_html)
+#         webbrowser.open_new_tab(f'file://{os.getcwd()}/Files_for_analysis/{FILE_HTML_1}')
+#
+# if __name__ == "__main__":
+#     PriceMachine.load_prices(FILE_PATH)
+#     PriceMachine().export_to_html()
 
-from airium import Airium
-import time
-import keyboard
+from tabulate import tabulate
 
 
-FILE_PATH = '../Price_List_Analyzer_Glob/Files_for_analysis'
-FILE_HTML = '../Price_List_Analyzer_Glob/Files_for_analysis/output.html'
-FILE_HTML_1 = 'output.html'
-LIST_RESULT = []
-SEARCH = input(f"Найти: ")
-SAFE_WORD = []
-class PriceMachine:
+def print_table_tabulate(data):
+    headers = ["ID", "Имя", "Возраст"]
+    formatted_data = [headers] + data
 
-    def load_prices(self):
-        '''
-            Сканирует указанный каталог. Ищет файлы со словом price в названии.
-            В файле ищет столбцы с названием товара, ценой и весом.
-            Допустимые названия для столбца с товаром:
-                товар
-                название
-                наименование
-                продукт
-            Допустимые названия для столбца с ценой:
-                розница
-                цена
-            Допустимые названия для столбца с весом (в кг.)
-                вес
-                масса
-                фасовка
-        '''
-        Product_name = ['товар', 'название', 'наименование', 'продукт']
-        Name_with_price = ['розница', 'цена']
-        Name_with_weight = ['вес', 'масса', 'фасовка']
-        files = os.listdir(FILE_PATH)
+    # Вывод в разных стилях
+    print("\nСтиль plain:")
+    print(tabulate(formatted_data, headers=headers, tablefmt="plain"))
 
-        for file in files:
+    print("\nСтиль grid:")
+    print(tabulate(formatted_data, headers=headers, tablefmt="grid"))
 
-            # if key == "exit":
-            #     break
-            if "price" in file:
-                with open(f'{FILE_PATH}/{file}', mode="r", encoding='utf-8') as sort_file:
-                    for row in csv.reader(sort_file):
-                        for i in Product_name:
-                            try:
-                                index_Product_name = row.index(i)
-                            except:
-                                None
-                        a = row[index_Product_name]
-
-                        for i in Name_with_price:
-                            try:
-                                index_Name_with_price = row.index(i)
-                            except:
-                                None
-                        try:
-                            b = int(row[index_Name_with_price])
-                        except:
-                            b = row[index_Name_with_price]
-
-                        for i in Name_with_weight:
-                            try:
-                                index_Name_with_weight = row.index(i)
-                            except:
-                                None
-                        try:
-                            c = int(row[index_Name_with_weight])
-                        except:
-                            c = row[index_Name_with_weight]
-
-                        if a not in Product_name and b not in Name_with_price and c not in Name_with_weight:
-                            result = a, b, c, file
-                            LIST_RESULT.append(result)
-        LIST_RESULT.sort(key=lambda x: x[1])
-        return LIST_RESULT
-
-    def find_text(self):
-        '''
-        Нолучает текст и возвращает список позиций,
-        содержащий этот текст в названии продукта.
-        <td 1></td><td Филе пангасиуса б/ш ></td><td 92></td><td 1></td><td price_5.csv></td>
-        <td 2></td><td Филе пангасиуса б/ш ></td><td 103></td><td 1></td><td price_4.csv></td>
-        <td 3></td><td Ряпушка вял н/р></td><td 119></td><td 1></td><td price_3.csv></td>
-        <td 4></td><td Килька п/п ></td><td 130></td><td 1></td><td price_0.csv></td>
-        '''
-        file = open(f"{FILE_HTML}", "a", encoding='utf-8')
-        b = Airium(source_minify=True)
-        y = 0
+    print("\nСтиль fancy_grid:")
+    print(tabulate(formatted_data, headers=headers, tablefmt="fancy_grid"))
 
 
-        for i in LIST_RESULT:
+# Пример использования
+data = [
+    [1, "Иван Петрович", 25],
+    [2, "Мария Ивановна", 30]
+]
 
-            # time.sleep(0.05)
-            if keyboard.is_pressed('e'):
-                SAFE_WORD.append('e')
-            if keyboard.is_pressed('x'):
-                SAFE_WORD.append('x')
-            if keyboard.is_pressed('i'):
-                SAFE_WORD.append('i')
-            if keyboard.is_pressed('t'):
-                SAFE_WORD.append('t')
-            if {'e', 'x', 'i', 't'} <= set(SAFE_WORD):
-                break
-
-            if SEARCH in i[0]:
-                y += 1
-                b.break_source_line()
-                with b.tr():
-                    with b.td(_t=y):
-                        pass
-                    with b.td(_t=i[0]):
-                        pass
-                    with b.td(_t=i[1]):
-                        pass
-                    with b.td(_t=i[2]):
-                        pass
-                    with b.td(_t=i[3]):
-                        pass
-        file.write(str(b))
-        return b
-
-    def export_to_html(self):
-        '''
-        <!DOCTYPE html>
-        <html>
-        <head>
-            <title>Позиции продуктов</title>
-        </head>
-        <body>
-            <table>
-                <tr>
-                    <th>Номер</th>
-                    <th>Название</th>
-                    <th>Цена</th>
-                    <th>Фасовка</th>
-                    <th>Файл</th>
-                    <th>Цена за кг.</th>
-                </tr>
-        '''
-        file = open(f"{FILE_HTML}", "w", encoding ='utf-8')
-        a = Airium()
-        a('<!DOCTYPE html>')
-        with ((((a.html())))):
-            a.head()
-            a.title(_t="Позиции продуктов")
-        with a.body():
-            with a.table():
-                with a.tr():
-                    a.th(_t="№")
-                    a.th(_t="Наименование")
-                    a.th(_t="Цена")
-                    a.th(_t="Масса")
-                    a.th(_t="Файл")
-                with a.tr(_t=self.find_text()):
-                    pass
-            file.write(str(a))
-        # webbrowser.open_new_tab(file_html)
-        webbrowser.open_new_tab(f'file://{os.getcwd()}/Files_for_analysis/{FILE_HTML_1}')
-
-if __name__ == "__main__":
-    PriceMachine.load_prices(FILE_PATH)
-    PriceMachine().export_to_html()
+print_table_tabulate(data)
