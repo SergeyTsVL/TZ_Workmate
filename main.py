cfc075892661430cb4d8002d38a503106325b6e8
SergeyTsVL <tsarskiytsarskiy@mail.ru>
2025-04-15 14:37:19 +0700

    1

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import os
-import csv
-from tabulate import tabulate
-import textwrap
+
 
 FILE_PATH = './logs'
+FILE_NAME = 'handlers.txt'
 LOGS_lIST = []
 SET_lIST = []
 END_lIST = []
@@ -12,126 +11,75 @@ TOTAL_INFO, TOTAL_ERROR, TOTAL_WARNING, TOTAL_DEBUG, TOTAL_CRITICAL, Total_reque
 
 class Analiz_logs():
     def load_file(self):
-        global SET_lIST, TOTAL_INFO, TOTAL_ERROR, TOTAL_WARNING, TOTAL_DEBUG, TOTAL_CRITICAL
-        Total_requests = 0
+        '''
+        Этот код читает все файлы в папке указанной в FILE_PATH. Если файл не читаемый выдает по нему исключение.
+        Прочитанные строки в файле преобразуются в списки, разделителем служит пробел. Далее уже элементы списков
+        подсчитываются для заполнения таблицы INFO, ERROR, ... и для расчета TOTAL_SUMM TOTAL_INFO, TOTAL_ERROR...
+        Далее строка с подробным описанием путем преобразования строк сокращается до, например, /api/v1/shipping/.
+        :return:
+        '''
+        global SET_lIST, TOTAL_INFO, TOTAL_ERROR, TOTAL_WARNING, TOTAL_DEBUG, TOTAL_CRITICAL, Total_requests
         files = os.listdir(FILE_PATH)
         for file in files:
-            with open(f'{FILE_PATH}/{file}', mode="r", encoding='utf-8') as file:
-                for line in file:
-                    Total_requests += 1
-                    LOGS_lIST.append([' '.join((line.split()[3:])), line.split()[2]])
-                    SET_lIST.append(' '.join((line.split()[3:])))
-        # END_lIST.append([f'Total requests:{Total_requests}'])
-        # END_lIST.append(['HANDLER', 'INFO', 'DEBUG', 'WARNING', 'ERROR', 'CRITICAL'])
+            try:
+                with open(f'{FILE_PATH}/{file}', mode="r", encoding='utf-8') as file:
+                    for line in file:
+                        Total_requests += 1
+                        LOGS_lIST.append([' '.join((line.split()[3:])), line.split()[2]])
+                        SET_lIST.append(' '.join((line.split()[3:])))
+            except:
+                print(f'{file} - Не доступен/корректен для обработки')
+        END_lIST.append(['HANDLER', 'INFO', 'DEBUG', 'WARNING', 'ERROR', 'CRITICAL'])
         SET_lIST = set(SET_lIST)
         SET_lIST = list(SET_lIST)
         SET_lIST = sorted(SET_lIST)
         for i in SET_lIST:
             INFO, ERROR, WARNING, DEBUG, CRITICAL = 0, 0, 0, 0, 0
-            for j in LOGS_lIST:
-                if i == j[0]:
-                    if j[1] == 'INFO':
-                        INFO += 1
-                        TOTAL_INFO += 1
-                    if j[1] == 'ERROR':
-                        ERROR += 1
-                        TOTAL_ERROR += 1
-                    if j[1] == 'WARNING':
-                        WARNING += 1
-                        TOTAL_WARNING += 1
-                    if j[1] == 'DEBUG':
-                        DEBUG += 1
-                        TOTAL_DEBUG += 1
-                    if j[1] == 'CRITICAL':
-                        CRITICAL += 1
-                        TOTAL_CRITICAL += 1
-
-
-            if '/' in i:
-                char_list = []
-                # print(char_list)
-                for ii, char in enumerate(i):
-                    # print(char)
-                    if char == "/":
-                        char_list.append(i.index(char))
-                        print(char_list)
-                print(type(char_list[0]), type(char_list[1]), type(char_list[2]))
-                i = i.split(i.index(min(char_list)))[1].split(i.index(max(char_list)))[0]
-
-
-            END_lIST.append([textwrap.fill(i, 25), INFO, DEBUG, WARNING, ERROR, CRITICAL])
-        END_lIST.append(['TOTAL_SUMM', TOTAL_INFO, TOTAL_ERROR, TOTAL_WARNING, TOTAL_DEBUG, TOTAL_CRITICAL])
-        # print(END_lIST)
-        # Запись в файл
-        with open('handlers.csv', 'w', newline='', encoding='utf-8') as file:
-            writer = csv.writer(file)
-            writer.writerows(END_lIST)
-
-        # Чтение файла для проверки
-        with open('handlers.csv', 'r', encoding='utf-8') as file:
-            reader = csv.reader(file)
-            # for row in reader:
-            #     print(row)
-
-
-
+            try:
+                for j in LOGS_lIST:
+                    if i == j[0] and '/' in j[0]:
+                        if j[1] == 'INFO':
+                            INFO += 1
+                            TOTAL_INFO += 1
+                        if j[1] == 'ERROR':
+                            ERROR += 1
+                            TOTAL_ERROR += 1
+                        if j[1] == 'WARNING':
+                            WARNING += 1
+                            TOTAL_WARNING += 1
+                        if j[1] == 'DEBUG':
+                            DEBUG += 1
+                            TOTAL_DEBUG += 1
+                        if j[1] == 'CRITICAL':
+                            CRITICAL += 1
+                            TOTAL_CRITICAL += 1
+                if '/' in i:
+                    char_list = []
+                    char_int = 0
+                    for char in i:
+                        char_int += 1
+                        if "/" == char:
+                            char_list.append(char_int)
+                    min_char = min(char_list)
+                    max_char = max(char_list)
+                    parts = i[min_char - 1:max_char]
+                    END_lIST.append([parts, INFO, DEBUG, WARNING, ERROR, CRITICAL])  # textwrap.fill(parts, 25)
+            except:
+                print(ValueError("Пустая подстрока"))
+        END_lIST.append(['TOTAL_SUMM', TOTAL_INFO, TOTAL_DEBUG, TOTAL_WARNING, TOTAL_ERROR, TOTAL_CRITICAL])
 
 def print_table_tabulate(data):
-    # headers = ['HANDLER', 'INFO', 'DEBUG', 'WARNING', 'ERROR', 'CRITICAL']
-    formatted_data = ['HANDLER', 'INFO', 'DEBUG', 'WARNING', 'ERROR', 'CRITICAL']
-
-    # Вывод в разных стилях
-    # print("\nСтиль plain:")
-    # print(tabulate(END_lIST, tablefmt="plain"))
-    #
-    # print("\nСтиль grid:")
-    # print(tabulate(formatted_data, headers=headers, tablefmt="grid"))
-    #
-    # print("\nСтиль fancy_grid:")
-    # print(tabulate(formatted_data, headers=headers, tablefmt="fancy_grid"))
-    # assign data
-    # a = [
-    #     ["Nikhil", "Delhi"],
-    #     ["Ravi", "Kanpur"],
-    #     ["Manish", "Ahmedabad"],
-    #     ["Prince", "Bangalore"]
-    # ]
-
-    # create header
-    header = ["Name", "City"]
-
-    # print header
-    # print(f"{header[0]:<10} {header[1]:<15}")
-    # print("-" * 25)
-    line_width = 20
-    # print rows
-    # for row in END_lIST:
-        # print(f"{textwrap.fill(row[0], 30):<20} {row[1]:<15}  {row[2]:<15}  {row[3]:<15}  {row[4]:<15}  {row[5]:<15}")
-
-        # print('{:<30}|{:<10}|{:<10}|{:<10}|{:<10}|{:<10}'.format( row[0], row[1], row[2], row[3], row[4], row[5]))
-
-        # print(f'{row[0]}'.ljust(line_width) + "LEFT"*6, f'row[1]'.ljust(line_width) + "LEFT"*6, f'row[2]'.ljust(line_width) + "LEFT"*6, f'row[3]'.ljust(line_width) + "LEFT"*6)
-
-# textwrap.fill(row[0], 20)
-# "{0:<{1}}{0}".format(row[1], 40)
-# Пример использования
-# data = [
-#     [1, "Иван Петрович", 25],
-#     [2, "Мария Ивановна", 30]
-# ]
-# # textwrap.fill(row[0], 25),
-
-
+    """
+    Этот метод создает файл с названием указанным в FILE_NAME. Записываем в него данные используя print(), при этом
+     определяем указанный формат.
+    :param data:
+    :return:
+    """
+    with open(FILE_NAME, 'w', newline='', encoding='utf-8') as file:
+        print(f'Total requests:{Total_requests}', file=file)
+        for row in data:
+            print('{:<30}{:<15}{:<15}{:<15}{:<15}{:<15}'.format( row[0], row[1], row[2], row[3], row[4], row[5]), file=file)
 
 f = Analiz_logs()
 f.load_file()
 print_table_tabulate(END_lIST)
-
-
-# print ('{:<20}{:<20}'.format('UserName:ывпастм','Foo'))
-# print ('{:<30}{:<40}'.format('User:','FooBar'))
-# print ('{:<30}{:<40}'.format('','FooBar42'))
-#
-# line_width = 20
-# print ("Username:".ljust(line_width) + "LEFT"*6)
-# print ("".ljust(line_width) + "RIGHT"*3)
